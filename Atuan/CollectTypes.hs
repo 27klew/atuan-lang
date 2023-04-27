8b95541dbcc67b2dae39662bee99215aa36a9d51
Kacper Lewandowski <kacper.lewandowski540@gmail.com>
2023-04-27 17:09:03 +0200

    TypeCollection

--- Atuan/CollectTypes.hs
+++ Atuan/CollectTypes.hs
@@ -1,12 +1,12 @@
 
-module Atuan.CollectTop where
+module Atuan.CollectTypes where
 
 import Atuan.Abs (Program'(..), Top'(..), TypeDef'(..), Ident(..), TVar'(..), Constr'(..), TVar, Type' (TypeFunc, TypeIdent, TypeApp, TypeInt, TypeBool, TypeList, TypeVar), TypeAnnot' (TypeAnnotation))
-import Control.Monad.Reader (Reader, ReaderT)
-import Control.Monad.Except (ExceptT, runExceptT, when, MonadError (throwError), unless)
+import Control.Monad.Reader (Reader, ReaderT, MonadReader (ask), MonadTrans (lift))
+import Control.Monad.Except (ExceptT, runExceptT, when, MonadError (throwError), unless, Except)
 import Data.Functor.Identity (Identity (runIdentity))
 import Control.Monad.State.Strict (modify, put, get, StateT, execStateT, runState, evalState, evalStateT)
-import Data.Map (Map, insert, empty, keys, member, elems, toList, fromList, union)
+import Data.Map (Map, insert, empty, keys, member, elems, toList, fromList, union, lookup)
 import Data.Char (isUpper, isLower)
 import GHC.Conc (TVar(TVar))
 import qualified Data.Set as Set (fromList)
@@ -15,26 +15,112 @@ import Data.Maybe (isNothing)
 -- import Prelude (Maybe(..), Eq, Either(..), String, Show, Bool, mapM_, (==), )
 
 
+
 -- TODO something other here
 -- data Type = Int | Bool | Func Type Type/
 
-type TypeEnv a = Map Ident ([TVar' a], [Constr' a])
 
-data ADTs a = ADTs {types :: Map Ident ([TVar' a], [Ident]), constr :: Map Ident (Type' a)}
+-- type TypeEnv a = Map Ident ([TVar' a], [Constr' a])
+
+
+data ADT a = ADT {name :: Ident, vars :: [TVar' a], constrs :: [Ident]} deriving Show 
+
+
+data ADTs a = ADTs {from_name :: Map Ident (ADT a) , from_constr :: Map Ident (Constr' a)}
+
+
+type SE a b = (StateT (ADTs b) (ExceptT String Identity)) a
+
+-- type RE a b = (ReaderT (ADTs b) (ExceptT String Identity)) a
+
+
+
+-- note :: e -> Maybe a -> Either e a
+-- note e Nothing  = Left e
+-- note _ (Just a) = Right a
+
+
+note :: e -> Maybe a -> Except e a
+note e Nothing  = throwError e
+note _ (Just a) = return a
+
+
+getConstr :: Ident -> SE (Constr' a) a
+getConstr i = do
+  adts <- get
+  constr <- lift $ note ("No such constructor" ++ show i) (Data.Map.lookup i (from_constr adts))
+  return constr
+
+
+getType :: Ident -> SE (ADT a) a
+getType i = do
+  adts <- get
+  adt <- lift $ note ("No such type " ++ show i) (Data.Map.lookup i (from_name adts))
+  return adt
+
+
 
 emptyADTs = ADTs empty  empty
 
 
--- type SS a = ReaderT TypeEnv (ExceptT String Identity) a
 
-type RE a b = (StateT (ADTs b) (ExceptT String Identity)) a
+collect :: Show a => Atuan.Abs.Program' a -> Either String (ADTs a)
+collect program = runIdentity (runExceptT (execStateT ( collectProgram program) emptyADTs))
 
 
--- isType :: Ident -> RE () b
--- isType i = do
---   env <- get
---   unless (member i env)
---     (throwError $ "Not a type: " ++ show i) 
+isType :: Top' a -> Bool
+isType (TopType _ _ ) = True
+isType _ = False
+
+topName :: Top' a -> Ident
+topName (TopType _ (TypeDefinition _ id _ _ )) = id
+-- topNamae (topDef _ _ ) TODO 
+
+
+collectProgram :: Show a => Atuan.Abs.Program' a -> SE () a
+collectProgram (Atuan.Abs.ProgramText ann tops) = do
+    let types = filter isType tops
+    let names = map topName types
+    let names' = sort names
+   
+
+    let x = findDuplicate names'
+    unless (isNothing x)
+      (throwError $ "Duplicate typename " ++ show x) -- TODO locations
+  
+
+    let y = filter (not . isUpperIdent) names'
+    unless (null x)
+      (throwError $ "Typename should start with capital letters:" ++ show x)
+
+    
+
+    mapM_ collectType types
+    types' <- get
+
+    -- TODO
+    -- let con = map snd (elems types)
+    -- let con' = map (\(DataConstructor _ id _) -> id) (concat con)
+    -- let con'' = sort con'
+    -- checkConstructors con''
+
+    return ()
+
+
+
+collectType :: Show a => Atuan.Abs.Top' a -> SE () a
+collectType (TopDef _ _ ) = return ()
+collectType (TopType pos (Atuan.Abs.TypeDefinition _ ident vars constr)) = do
+    -- checkTypeName ident
+    mapM_ checkTypeVar vars
+    mapM_ (checkDataConstr ident vars) constr
+    constr' <-  mapM identToVarConstr constr
+
+    m <- get
+    
+    --  TODO checkIdentUnique ident m
+
+    addType ident vars constr'
 
 
 identConstr :: Constr' a -> Ident
@@ -42,6 +128,35 @@ identConstr (DataConstructor _ i _) = i
 
 identType :: Constr' a -> Type' a
 identType (DataConstructor _ _ (TypeAnnotation _ t)) = t
+
+
+
+addConstr :: Constr' a -> SE () a
+addConstr constr = do
+  (ADTs types con) <- get
+  let name = identConstr constr
+  let con' = insert name constr con 
+  put (ADTs types con')
+
+
+addType :: Ident -> [TVar' a] -> [Constr' a]  -> SE () a
+addType  name vars constr = do
+  (ADTs types con) <- get
+
+  let conide = map identConstr constr 
+
+  let adt = ADT name vars conide  
+
+  let types' = insert name adt types
+
+  put (ADTs types' con)
+
+  mapM_ addConstr constr
+
+
+
+
+
 
 
 -- isConstr ::  Ident -> RE Ident b
@@ -59,8 +174,6 @@ identType (DataConstructor _ _ (TypeAnnotation _ t)) = t
 
 
 
-collect :: Show a => Atuan.Abs.Program' a -> Either String (ADTs a)
-collect program = runIdentity (runExceptT (execStateT ( collectProgram program) emptyADTs))
 
 
 
@@ -73,25 +186,25 @@ findDuplicate (x:y:xs) =
 
 
 
-checkConstructors :: [Ident] -> RE () a
+checkConstructors :: [Ident] -> SE () a
 checkConstructors con = do
   let dup = findDuplicate con
   unless (isNothing dup)
    ( throwError $ "Data constructors should be unique: " ++ show dup )
 
 
-collectProgram :: Show a => Atuan.Abs.Program' a -> RE () a
-collectProgram (Atuan.Abs.ProgramText ann tops) = do
-    mapM_ collectType tops
-    types <- get
+-- collectProgram :: Show a => Atuan.Abs.Program' a -> RE () a
+-- collectProgram (Atuan.Abs.ProgramText ann tops) = do
+--     mapM_ collectType tops
+--     types <- get
 
-    -- TODO
-    -- let con = map snd (elems types)
-    -- let con' = map (\(DataConstructor _ id _) -> id) (concat con)
-    -- let con'' = sort con'
-    -- checkConstructors con''
+--     -- TODO
+--     -- let con = map snd (elems types)
+--     -- let con' = map (\(DataConstructor _ id _) -> id) (concat con)
+--     -- let con'' = sort con'
+--     -- checkConstructors con''
 
-    return ()
+--     return ()
 
 
 isUpperIdent :: Ident -> Bool
@@ -101,7 +214,7 @@ isLowerIdent :: Ident -> Bool
 isLowerIdent (Ident ident) = isLower (head ident)
 
 
-checkTypeName :: Show a => Ident  -> RE () a
+checkTypeName :: Show a => Ident  -> SE () a
 checkTypeName ident = do
     unless (isUpperIdent ident)
          (throwError $  "Error: type names should begin with capital letters. "
@@ -109,7 +222,7 @@ checkTypeName ident = do
          )
 
 
-checkTypeVar :: Show a => TVar' a -> RE () a
+checkTypeVar :: Show a => TVar' a -> SE () a
 checkTypeVar (TypeVariable pos ident)  = do
     unless (isLowerIdent ident)
         (throwError $ "Error: type variables should begin with small letter."
@@ -122,7 +235,7 @@ getResultType (TypeFunc _ t1 t2) = getResultType t2
 getResultType t = t
 
 
-checkDataConstr :: Show a => Ident -> [TVar' a] -> Constr' a -> RE () a
+checkDataConstr :: Show a => Ident -> [TVar' a] -> Constr' a -> SE () a
 checkDataConstr id vars (DataConstructor pos ident (TypeAnnotation _ ty)) = do
     checkTypeName ident
     let t = getResultType ty
@@ -147,7 +260,7 @@ checkDataConstr id vars (DataConstructor pos ident (TypeAnnotation _ ty)) = do
 
 
 
-identToVar :: Show a => Type' a -> RE (Type' a) a
+identToVar :: Show a => Type' a -> SE (Type' a) a
 identToVar x = case x of
   TypeInt a -> return $ TypeInt a
   TypeBool a -> return $ TypeBool a
@@ -176,47 +289,21 @@ identToVar x = case x of
     return $ TypeVar a id
 
 
-identToVarConstr :: Show a => Constr' a -> RE (Constr' a) a
+identToVarConstr :: Show a => Constr' a -> SE (Constr' a) a
 identToVarConstr (DataConstructor pos ident (TypeAnnotation ps ty)) = do
     ty' <- identToVar ty
     return (DataConstructor pos ident (TypeAnnotation ps ty'))
 
 
-checkIdentUnique :: Ident -> Map Ident a -> RE () b
+checkIdentUnique :: Ident -> Map Ident a -> SE () b
 checkIdentUnique i m =
   when (member i m)
     (throwError $ "Typename " ++ show i ++ "is not unique")
 
 
 
-addType :: Ident -> [TVar' a] -> [Constr' a]  -> RE () a
-addType  name vars constr = do
-  (ADTs types con) <- get
-
-  let conide = map identConstr constr 
-  let contypes = map identType constr 
-
-  let types' = insert name (vars, conide) types
-
-  let mm = fromList (zip conide contypes)
-
-  let con' = union mm con
-  
-  put (ADTs types' con')
 
 
-collectType :: Show a => Atuan.Abs.Top' a -> RE () a
-collectType (TopDef _ _ ) = return ()
-collectType (TopType pos (Atuan.Abs.TypeDefinition _ ident vars constr)) = do
 
-    checkTypeName ident
-    mapM_ checkTypeVar vars
-    mapM_ (checkDataConstr ident vars) constr
-    constr' <-  mapM identToVarConstr constr
 
-    m <- get
-    
-    --  TODO checkIdentUnique ident m
-
-    addType ident vars constr'
 
--- Atuan/Interpreter.hs
+++ Atuan/Interpreter.hs
@@ -18,20 +18,20 @@ import Prelude
   , IO, (>>), (>>=), mapM_, putStrLn
   , FilePath
   , getContents, readFile, print
-  , map
+  , map, Maybe (..)
   )
 import System.Environment ( getArgs )
 import System.Exit        ( exitFailure )
 import Control.Monad      ( when )
 
-import Atuan.Abs   (Program, TVar', Constr', Ident)
+import Atuan.Abs   (Program, TVar' (TypeVariable), Constr' (DataConstructor), Ident (..), Type' (TypeVar), TypeAnnot' (TypeAnnotation))
 import Atuan.Lex   ( Token, mkPosToken )
 import Atuan.Par   ( pProgram, myLexer )
 import Atuan.Print ( Print, printTree )
 import Atuan.Skel  ()
-import Atuan.CollectTop ( collect , TypeEnv )
-import Data.List
-import Data.Map (elems, toList)
+import Atuan.CollectTypes ( collect , ADTs (ADTs, from_name, from_constr), ADT (ADT) )
+import Data.List ( (++), map, concat, unlines, intercalate )
+import Data.Map (elems, toList, keys, Map, lookup)
 
 
 
@@ -47,17 +47,53 @@ runFile :: Verbosity -> ParseFun Program -> FilePath -> IO ()
 runFile v p f = putStrLn f >> readFile f >>= run v p
 
 
-showType :: Show a => (Ident, ([TVar' a], [Constr' a])) -> String
-showType (ident, (vars, contrs)) = "Type: " ++ show ident ++ "\n\tvars: " ++ show vars  ++ "\n\tconstr: " ++ show contrs 
+-- showType :: Show a => (Ident, ([TVar' a], [Constr' a])) -> String
 
-showMap :: Show a =>  TypeEnv a -> String
-showMap x = intercalate  ndashes (map showType (toList x)) 
+-- showType :: Show a => Ident -> Map Ident ([TVar' a], [Ident]) -> Map Ident (Type' a) -> String
+-- showType ident types constrs = "aaa"
+--     -- "Type: " ++ show ident ++ "\n\tvars: " ++ show vars  ++ "\n\tconstr: " ++ show contrs
+
+showVars :: [TVar' a ] -> String
+showVars vars =
+  show $ map (\(TypeVariable _ (Ident n)) -> n) vars
+
+
+showConstructor (DataConstructor _ (Ident n) (TypeAnnotation _ t)) = 
+    "cname: " ++ n ++ 
+    "\n\ttype: " ++  show t
+
+
+showConstructors :: Show a => [Ident] -> ADTs a -> String
+showConstructors con types =
+  intercalate "\n" $
+      map ((\(Just (d)) -> showConstructor d) . (\x -> lookup x (from_constr types))) con
+
+
+showType :: Show a =>  ADTs a -> Ident -> String
+showType types name =
+    let Just (ADT (Ident name') vars con) = lookup name (from_name types) in
+      "{\nname: " ++ name'  ++
+      "\nvars: " ++ showVars vars ++
+      "\ncons: \n" ++ showConstructors con types ++ "\n}"
+
+    -- show (Data.Map.lookup name (from_name types))
+
+
+showTypes :: Show a =>  ADTs a -> String
+showTypes (ADTs types constr) =
+  "ADTs: " ++ intercalate ", " (map show (keys types)) ++ "\n\n"
+    ++ intercalate ndashes  (map (showType (ADTs types constr)) (keys types))
+
+
+    -- ++ ndashes ++ intercalate ndashes (map showType ((keys types) types constr))
+
+  -- intercalate ndashes (map showType (toList x)) 
 
 
 dashes = "--------------------------------------------------------------------------"
 ndashes = "\n" ++ dashes ++ "\n"
 
-ndash s = ndashes ++ dashes ++ "\n\n\t\t\t\t" ++ s ++ "\n" ++ dashes ++ ndashes 
+ndash s = ndashes ++ dashes ++ "\n\n\t\t\t\t" ++ s ++ "\n" ++ dashes ++ ndashes
 
 
 
@@ -74,12 +110,12 @@ run v p s =
       putStrLn "\nParse Successful!"
       showTree v tree
       putStrLn "\nSummary!"
-      
+
       case collect tree of
         Left str -> putStrLn $ "error: " ++ str
-        Right map -> 
-          putStrLn $ 
-            ndash "types"  ++ showMap map
+        Right map ->
+          putStrLn $
+            ndash "types"  ++ showTypes map
 
 
 
