f6892e252e731d6b765c21ee4f743f19dca36b20
Kacper Lewandowski <kacper.lewandowski540@gmail.com>
2023-06-01 10:35:38 +0200

    reorganized tests

---
+++ bad/type_mismatch_match.hs
@@ -0,0 +1,14 @@
+
+data Tree a where
+	Node :: a -> (Tree a) -> (Tree a) -> (Tree a)
+	Leaf :: (Tree a)
+	;
+
+
+a = Node 5 Leaf Leaf;
+
+b = match a with
+	(Leaf) >>> True,
+	(Node x r l) >>> x;
+
+main = b;
