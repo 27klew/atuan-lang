d3b3698bf5f41ea187e5f1128b415e9f1cd07fa6
Kacper Lewandowski <kacper.lewandowski540@gmail.com>
2023-06-06 20:30:51 +0200

    hanging examples

--- tester.sh
+++ tester.sh
@@ -4,13 +4,13 @@ for filename in good/*.hs; do
     base=${name%.hs}
 	echo "$base"
 	
-	timeout 2 ./Atuan/Interpreter  "$filename" > "good/$base.out"
+	# timeout 2 ./Atuan/Interpreter  "$filename" > "good/$base.out"
 	
-	# timeout 2 ./Atuan/Interpreter  "$filename" > a.out
+	timeout 3 ./Atuan/Interpreter  "$filename" > a.out
 
-	# if !(diff a.out "good/$base.out"); then
-	# 	exit 1
-	# fi
+	if !(diff a.out "good/$base.out"); then
+		exit 1
+	fi
 
 
 done
@@ -22,14 +22,14 @@ for filename in bad/*.hs; do
     base=${name%.hs}
 	echo "$base"
 	
-	# timeout 5 ./Atuan/Interpreter  "$filename" > "bad_out/$base.out"
+	# timeout 2 ./Atuan/Interpreter  "$filename" > "bad/$base.out"
 	
-	timeout 2 ./Atuan/Interpreter  "$filename" > a.out
+	timeout 3 ./Atuan/Interpreter  "$filename" > a.out
 
-	# diff a.out "good_out/$base.out"
 
-	if !(diff a.out "bad_out/$base.out"); then
+	if !(diff a.out "bad/$base.out"); then
 		exit 1
 	fi
-
 done
+
+rm a.out
