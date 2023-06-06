6afbbadcfd869412b81946c0dde6d55d92ee268f
Kacper Lewandowski <kacper.lewandowski540@gmail.com>
2023-06-06 18:12:21 +0200

    tests cleanup

--- good/let_shadow.hs
+++ good/let_shadow.hs
@@ -3,7 +3,9 @@ x  = 3;
 
 y = x + 1;
 
-x = x + 1;
+
+-- This should be ignored.
+x = x + 10000;
 
 
 main = y;
--- good/letrec.hs
+++ good/letrec.hs
@@ -1,2 +1,4 @@
+-- This shoudl type correctly to int,
+-- but should hang during exetution.
 y = letrec x = x + 1 in x;
 main = y;
