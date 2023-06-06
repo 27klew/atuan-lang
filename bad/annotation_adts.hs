e403081fc42a4d5f578d943107505602e699e82f
Kacper Lewandowski <kacper.lewandowski540@gmail.com>
2023-06-06 12:31:20 +0200

    more tests on annotations

---
+++ bad/annotation_adts.hs
@@ -0,0 +1,20 @@
+
+
+id (x :: Int) :: Int = x + 1;
+
+
+data A3 where 
+	Make :: A3;
+
+y = Make;
+
+-- Tutaj A3 jest typem (obecnie nie jest sprawdzane czy typy które są
+-- 	w annotacjach istnieją, ale jeśli użytkownik taki poda to i tak powstanie błąd typów
+-- )
+id2 (x :: A3) :: A3 = x;
+
+
+id3 (x :: A3) :: A3 = y;
+
+
+main = id2 3;
